cb5210126a49ab78bea3d190bfc2c928030a67bf
Sean Zellmer <sean@lejeunerenard.com>
2015-02-18 13:53:09 -0600

    Added .jshintrc, Gruntfile.js, package.json and bower.json

---
+++ Gruntfile.js
@@ -0,0 +1,68 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+
+        // Import package manifest
+        pkg: grunt.file.readJSON("bower.json"),
+
+        // Banner definitions
+        meta: {
+            banner: "/*\n" +
+                " *  <%= pkg.title || pkg.name %> - v<%= pkg.version %>\n" +
+                " *  <%= pkg.description %>\n" +
+                " *  <%= pkg.homepage %>\n" +
+                " *\n" +
+                " *  Made by <%= pkg.authors[0] %>\n" +
+                " *  Under <%= pkg.license %> License\n" +
+                " */\n"
+        },
+
+        // Concat definitions
+        concat: {
+            dist: {
+                src: ["src/form_functions.js"],
+                dest: "dist/form_functions.js"
+            },
+            options: {
+                banner: "<%= meta.banner %>"
+            }
+        },
+
+        // Lint definitions
+        jshint: {
+            files: ["src/form_functions.js"],
+            options: {
+                jshintrc: ".jshintrc"
+            }
+        },
+
+        // Minify definitions
+        uglify: {
+            my_target: {
+                src: ["dist/form_functions.js"],
+                dest: "dist/form_functions.min.js"
+            },
+            options: {
+                banner: "<%= meta.banner %>"
+            }
+        },
+
+        // watch for changes to source
+        // Better than calling grunt a million times
+        // (call 'grunt watch')
+        watch: {
+            jshint: {
+                files: ['src/*'],
+                tasks: ['default']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-contrib-concat");
+    grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+
+    grunt.registerTask("default", ["jshint", "concat", "uglify"]);
+    grunt.registerTask("travis", ["jshint"]);
+};
